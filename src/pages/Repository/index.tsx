0e40f35b1b0314d3306680ba68cba5a5378464ca
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-14 21:59:03 -0300

    Adds proper link for navigating to repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  name: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <h1>
+        Repository
+        {params.name}
+      </h1>
+    </>
+  );
 };
 
 export default Repository;
